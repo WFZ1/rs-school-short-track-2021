256acf9a310be7f627609310edc22a6f4377c41d
WFZ1 <pavelskumenko@gmail.com>
2021-04-09 14:18:39 +0300

    feat: add the solution of the task 07-file-names

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,25 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const countFiles = {};
+  names.forEach((el) => { countFiles[el] = 0; });
+
+  const newArr = [];
+
+  for (let i = 0; i < names.length; i++) {
+    if (newArr.includes(names[i])) {
+      countFiles[names[i]] += 1;
+    }
+
+    if (countFiles[names[i]] > 0) {
+      newArr[i] = `${names[i]}(${countFiles[names[i]]})`;
+    } else {
+      newArr[i] = names[i];
+    }
+  }
+
+  return newArr;
 }
 
 module.exports = renameFiles;
