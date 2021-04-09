1a263288df55bb35bcd6df539b16f0db77133294
WFZ1 <pavelskumenko@gmail.com>
2021-04-09 11:34:57 +0300

    feat: add the solution of the task 04-mine-sweeper

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,37 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const newMatrix = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    newMatrix[i] = [];
+
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j]) newMatrix[i][j] = 1;
+      else {
+        let k = 0;
+
+        // Top & Bottom
+        if (i > 0 && matrix[i - 1][j]) k++;
+        if (i < matrix.length - 1 && matrix[i + 1][j]) k++;
+
+        // Left & Right
+        if (j > 0 && matrix[i][j - 1]) k++;
+        if (j < matrix[0].length - 1 && matrix[i][j + 1]) k++;
+
+        // Top left, top right, bottom left, bottom right angles
+        if (i > 0 && j > 0 && matrix[i - 1][j - 1]) k++;
+        if (i > 0 && j < matrix[0].length - 1 && matrix[i - 1][j + 1]) k++;
+        if (i < matrix.length - 1 && j > 0 && matrix[i + 1][j - 1]) k++;
+        if (i < matrix.length - 1 && j < matrix[0].length - 1 && matrix[i + 1][j + 1]) k++;
+
+        newMatrix[i][j] = k;
+      }
+    }
+  }
+
+  return newMatrix;
 }
 
 module.exports = minesweeper;
