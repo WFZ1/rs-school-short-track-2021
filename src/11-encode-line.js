5e5bf002819896ef247286bb86e75dc4dd27b8aa
WFZ1 <pavelskumenko@gmail.com>
2021-04-11 22:09:25 +0300

    feat: add the solution of the task 11-encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,33 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+
+function encodeLine(str) {
+  /* const countLetters = {};
+
+  for (let i = 0; i < str.length; i++) {
+    countLetters[str[i]] = (countLetters[str[i]] ? countLetters[str[i]] : 0) + 1;
+  }
+
+  let newStr = '';
+  Object.keys(countLetters).forEach((letter) => {
+    newStr += (countLetters[letter] === 1) ? letter : countLetters[letter] + letter;
+  }); */
+
+  let newStr = '';
+
+  for (let i = 1, k = 1; i <= str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      k += 1;
+    } else if (k > 1) {
+      newStr += k + str[i - 1];
+      k = 1;
+    } else {
+      newStr += str[i - 1];
+    }
+  }
+
+  return newStr;
 }
 
 module.exports = encodeLine;
